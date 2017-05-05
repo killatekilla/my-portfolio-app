8cf5b170ddda501b2255385cf565c79dac6cca6b
Nik Latkin <nlatkin@nextradioapp.com>
2017-05-04 21:03:31 -0500

    Integrated new action to portfolio feature

--- app/controllers/porfolios_controller.rb
+++ app/controllers/porfolios_controller.rb
@@ -1,5 +1,21 @@
 class PorfoliosController < ApplicationController
   def index
-    @portfolio_items = Porfolio.all
+    @portfolio_item = Porfolio.all
+  end
+
+  def new
+  	@portfolio_item = Porfolio.new
+  end
+
+  def create
+  	@portfolio_item = Porfolio.new(params.require(:porfolio).permit(:title, :subtitle, :body))
+
+  	respond_to do |format|
+  		if @portfolio_item.save
+  			format.html { redirect_to @portfolio_item, notice: 'Your portfolio item was created' }
+  		else
+  			format.html { render :new }
+  		end
+  	end
   end
 end
